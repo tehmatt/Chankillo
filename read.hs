ca13f7cdceae794080110516cb9d4804c7ac1b41
alphaq <snitrama@gmail.com>
2013-07-21 01:44:07 -0400

    Basic framework of REPL should be in place. Hopefully.

--- read.hs
+++ read.hs
@@ -1,3 +1,11 @@
 :m System.IO
 
+prompt = ">>> "
 
+readEvalPrintLoop :: IO ()
+readEvalPrintLoop = do
+	maybeLine <- getLine
+	case maybeLine of
+		Nothing -> return ()
+		Just "exit" -> return ()
+		Just line -> putStr "Input was " ++ line ++ "\n"
