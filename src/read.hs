cac93a72ff2f4d525164e6483c27a4888dd455c9
Matt Bryant <mbryant@andrew.cmu.edu>
2013-07-21 13:39:29 -0400

    Renamed repl function, flush buffer at prompt, resugared

--- src/read.hs
+++ src/read.hs
@@ -1,23 +1,24 @@
-module Read (history, readEvalPrintLoop) where
+module Read (history, repl) where
 	import System.IO
 
-	prompt = ">>> " :: String
+	prompt :: IO String
+	prompt = do
+		putStr ">>> "
+		hFlush stdout
+		getLine
 
 	history = [] :: [String]
 
 	prepend :: Monad m => a -> [a] -> m [a]
-	prepend x xs = case xs of
-		[] -> return [x]
-		xs -> return (x:xs)
+	prepend x [] = return [x]
+	prepend x xs = return (x:xs)
 
-	readEvalPrintLoop :: IO ()
-	readEvalPrintLoop =
-		putStr prompt >>
-		getLine >>= \maybeLine ->
-			case maybeLine of
-				"exit" -> return ()
-				"\EOT" -> return ()
-				line ->
-					prepend line history >>= \history ->
-					putStrLn ("Input was " ++ line) >>
-					readEvalPrintLoop
+	repl :: IO ()
+	repl = do
+		maybeLine <- prompt;
+		case maybeLine of
+			"exit" -> return ()
+			"\EOT" -> return ()
+			line ->
+				prepend line history >>= \history -> putStrLn ("Input was " ++ line) >>
+				repl
