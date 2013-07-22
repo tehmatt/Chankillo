8d7b24e875c85857a6277c85544ba8525ec677f5
alphaq <snitrama@gmail.com>
2013-07-21 20:22:18 -0400

    Added keyword list and function to read in keyword list.

--- src/parse.hs
+++ src/parse.hs
@@ -1,5 +1,11 @@
 module Parse (parse, validLineEnding) where
 	import Data.List
+	import Data.List.Split
+
+	keywords :: IO [String]
+	keywords = do
+		wordlist <- readFile "../resources/keywords"
+		return splitOn("\n") wordlist
 
 	parse :: [String] -> [String]
 	parse lines = lines
